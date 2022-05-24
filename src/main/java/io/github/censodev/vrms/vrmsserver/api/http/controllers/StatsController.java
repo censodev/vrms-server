56dd740e6ca10da7285eae1c1cc6c1511d8868d6
censodev <buiphuongaptutc@gmail.com>
2022-05-24 23:53:53 +0700

    remove stats functions

--- src/main/java/io/github/censodev/vrms/vrmsserver/api/http/controllers/StatsController.java
+++
@@ -1,9 +0,0 @@
-package io.github.censodev.vrms.vrmsserver.api.http.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("api/stats")
-public class StatsController {
-}
